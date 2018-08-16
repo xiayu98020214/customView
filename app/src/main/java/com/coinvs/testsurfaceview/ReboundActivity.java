7f3a59cdd50186f67fd0b723a62f4432bed1307b
xiayu <xiayu98020214@sina.com>
2018-08-16 09:56:55 +0800

    呼吸灯

--- app/src/main/java/com/coinvs/testsurfaceview/ReboundActivity.java
+++ app/src/main/java/com/coinvs/testsurfaceview/ReboundActivity.java
@@ -2,6 +2,7 @@ package com.coinvs.testsurfaceview;
 
 import android.animation.Animator;
 import android.animation.ObjectAnimator;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -83,6 +84,14 @@ public class ReboundActivity extends AppCompatActivity {
 
             }
         });
+
+        BreatheView brv = (BreatheView) findViewById(R.id.brv);
+        brv.setInterval(2000) //设置闪烁间隔时间
+                .setCoreRadius(5f)//设置中心圆半径
+                .setDiffusMaxWidth(300f)//设置闪烁圆的最大半径
+                .setDiffusColor(Color.parseColor("#0cf465"))//设置闪烁圆的颜色
+                .setCoreColor(Color.parseColor("#f40c3a"))//设置中心圆的颜色
+                .onStart();
     }
 
 }
